57162729d8d2c0e358522d4956270d8fe16835bc
Panji Angkasa Putra <133610889+pnjia@users.noreply.github.com>
2024-09-26 04:58:27 +0700

    Update tugasNotasiBigO.java

--- tugasNotasiBigO.java
+++ tugasNotasiBigO.java
@@ -17,12 +17,13 @@ public class tugasNotasiBigO {
 //        pada program tersebut menggunakan notasi big O(n)
 //        kenapa?, karena input dengan iterasi yang dilakukan adalah sama.
 //    2.  Simulasi Perhitungan Data
-//        menghitung n^2, untuk n = 1, 2, 3, 4, 5
+//        n^2
+//        menghitung 5^2, untuk iterasi = 1, 2, 3, 4, 5
 //        simulasinya:
-//        untuk n = 1, maka hasil: 0 + 5 = 5
-//        untuk n = 2, maka hasil: 5 + 5 = 10
-//        untuk n = 3, maka hasil: 10 + 5 = 15
-//        untuk n = 4, maka hasil: 15 + 5 = 20
-//        untuk n = 5, maka hasil: 20 + 5 = 25
+//        untuk iterasi ke-1, maka hasil: 0 + 5 = 5
+//        untuk iterasi ke-2, maka hasil: 5 + 5 = 10
+//        untuk iterasi ke-3, maka hasil: 10 + 5 = 15
+//        untuk iterasi ke-4, maka hasil: 15 + 5 = 20
+//        untuk iterasi ke-5, maka hasil: 20 + 5 = 25
     }
 }
